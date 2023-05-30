6f971f8ce18f7b1bde62601b82ded159fa9368d4
long2ice <long2ice@gmail.com>
2023-05-31 00:16:38 +0800

    feat: improve scheduler

--- meilisync_admin/scheduler.py
+++ meilisync_admin/scheduler.py
@@ -1,6 +1,6 @@
 import asyncio
 from asyncio import Task
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Tuple
 
 from loguru import logger
 from meilisync.discover import get_progress
@@ -14,38 +14,36 @@ from meilisync_admin.models import Source, Sync, SyncLog
 from meilisync_admin.settings import settings
 
 
-class Scheduler:
-    _tasks: Dict[int, Task] = {}
-    _save_task: Optional[Task] = None
-
-    @classmethod
-    async def startup(cls):
-        sources = await Source.all()
-        for source in sources:
-            await cls._start_source(source)
-
-    @classmethod
-    async def _start_source(cls, source: Source, reset_progress: bool = False):
-        stats: Dict[int, Dict[EventType, int]] = {}
-        lock = asyncio.Lock()
-        progress = get_progress(ProgressType.redis)(
+class Runner:
+    def __init__(self, source: Source):
+        self.current_progress = None
+        self.lock = None
+        self.queue = None
+        self.source = source
+        self.source_obj = None
+        self.progress = get_progress(ProgressType.redis)(
             dsn=settings.REDIS_URL, key=f"meilisync:progress:{source.pk}"
         )
-        if reset_progress:
-            await progress.reset()
-        current_progress = await progress.get()
-        collections_map: Dict[SyncSettings, EventCollection] = {}
-        tables_sync_settings_map: Dict[str, List[Tuple[SyncSettings, Sync]]] = {}
-        tables_map_reverse = {}
-        meili_map: Dict[SyncSettings, Tuple[Meili, int]] = {}
-        sync_settings = []
+        self.collections_map: Dict[SyncSettings, EventCollection] = {}
+        self.tables_sync_settings_map: Dict[str, List[Tuple[SyncSettings, Sync]]] = {}
+        self.tables_map_reverse: Dict[int, str] = {}
+        self.meili_map: Dict[SyncSettings, Tuple[Meili, int]] = {}
+        self.sync_settings: List[SyncSettings] = []
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    async def __aenter__(self):
+        self.lock = asyncio.Lock()
+        self.queue = asyncio.Queue()
+        self.stats: Dict[int, Dict[EventType, int]] = {}
         syncs = (
-            await Sync.filter(enabled=True, source=source)
+            await Sync.filter(enabled=True, source=self.source)
             .all()
             .select_related("meilisearch")
         )
         for sync in syncs:
-            tables_map_reverse[sync.pk] = sync.table
+            self.tables_map_reverse[sync.pk] = sync.table
             sync_setting = SyncSettings(
                 table=sync.table,
                 pk=sync.primary_key,
@@ -53,129 +51,149 @@ class Scheduler:
                 index=sync.index,
                 fields=sync.fields,
             )
-            tables_sync_settings_map.setdefault(sync.table, []).append(
+            self.tables_sync_settings_map.setdefault(sync.table, []).append(
                 (sync_setting, sync)
             )
-            collections_map[sync_setting] = EventCollection()
-            meili_map[sync_setting] = (
+            self.collections_map[sync_setting] = EventCollection()
+            self.meili_map[sync_setting] = (
                 sync.meili_client,
                 sync.meilisearch.insert_interval,
             )
-            sync_settings.append(
+            self.sync_settings.append(
                 sync_setting,
             )
-        source_obj = source.get_source(
-            current_progress, list(tables_sync_settings_map.keys())
+        self.source_obj = self.source.get_source(
+            self.current_progress, list(self.tables_sync_settings_map.keys())
         )
 
-        async def start_interval(m: Meili, c: EventCollection, interval: int):
-            while True:
-                await asyncio.sleep(interval)
-                try:
-                    async with lock:
-                        await m.handle_events(c)
-                        await progress.set(**current_progress)
-                except Exception as e:
-                    logger.error(f"Error when insert data to Meilisearch: {e}")
-
-        for ss in sync_settings:
-            meili, insert_interval = meili_map[ss]
+        for ss in self.sync_settings:
+            meili, insert_interval = self.meili_map[ss]
             if ss.full and not await meili.index_exists(ss.index_name):
-                data = await source_obj.get_full_data(ss)
+                data = await self.source_obj.get_full_data(ss)
                 if data:
                     await meili.add_full_data(ss.index_name, ss.pk, data)
                     logger.info(
-                        f'Full data sync for table "{source.label}.{ss.table}" '
+                        f'Full data sync for table "{self.source.label}.{ss.table}" '
                         f"done! {len(data)} documents added."
                     )
                 else:
                     logger.info(
-                        f'Full data sync for table "{source.label}.{ss.table}" '
+                        f'Full data sync for table "{self.source.label}.{ss.table}" '
                         "done! No data found."
                     )
             if insert_interval:
                 asyncio.ensure_future(
-                    start_interval(meili, collections_map[ss], insert_interval)
+                    self.start_interval(
+                        meili, self.collections_map[ss], insert_interval
+                    )
                 )
+        return self
 
-        async def sync_data():
-            logger.info(
-                f'Start increment sync data from "{source.label}" to'
-                f' Meilisearch, tables: {", ".join(tables_sync_settings_map.keys())}...'
-            )
-            async for event in source_obj:
-                if settings.DEBUG:
-                    logger.debug(event)
-                nonlocal current_progress
-                current_progress = event.progress
-                if not isinstance(event, Event):
-                    await progress.set(**current_progress)
-                    continue
-                ss_list = tables_sync_settings_map.get(event.table)
-                if not ss_list:
-                    continue
-                async with lock:
-                    for setting, sync_model in ss_list:
-                        m, _ = meili_map[setting]
-                        meilisearch = sync_model.meilisearch
-                        stats.setdefault(sync_model.pk, {}).setdefault(event.type, 0)
-                        stats[sync_model.pk][event.type] += 1
-                        if (
-                            not meilisearch.insert_size
-                            and not meilisearch.insert_interval
-                        ):
-                            await m.handle_event(event, setting)
-                            await progress.set(**current_progress)
-                        else:
-                            collection = collections_map[setting]
-                            collection.add_event(setting, event)
-                            if collection.size >= meilisearch.insert_size:
-                                await m.handle_events(collection)
-                                await progress.set(**current_progress)
-
-        async def save_stats():
-            while True:
-                await asyncio.sleep(60)
-                async with lock:
-                    objs = []
-                    for sync_id, events in stats.items():
-                        total = 0
-                        for event_type, count in events.items():
-                            total += count
-                            objs.append(
-                                SyncLog(sync_id=sync_id, count=count, type=event_type)
-                            )
-                        stats_str = ", ".join(
-                            f"{event_type.name}: {count}"
-                            for event_type, count in events.items()
+    async def save_stats(self):
+        while True:
+            await asyncio.sleep(60)
+            async with self.lock:
+                objs = []
+                for sync_id, events in self.stats.items():
+                    total = 0
+                    for event_type, count in events.items():
+                        total += count
+                        objs.append(
+                            SyncLog(sync_id=sync_id, count=count, type=event_type)
                         )
-                        logger.info(
-                            f'Save {total} sync logs for table "{source.label}'
-                            f'.{tables_map_reverse[sync_id]}", {stats_str}'
-                        )
-                    if not objs:
-                        continue
-                    await SyncLog.bulk_create(objs)
-                    stats.clear()
+                    stats_str = ", ".join(
+                        f"{event_type.name}: {count}"
+                        for event_type, count in events.items()
+                    )
+                    logger.info(
+                        f'Save {total} sync logs for table "{self.source.label}'
+                        f'.{self.tables_map_reverse[sync_id]}", {stats_str}'
+                    )
+                if not objs:
+                    continue
+                await SyncLog.bulk_create(objs)
+                self.stats.clear()
 
-        cls._tasks[source.pk] = asyncio.ensure_future(sync_data())
-        cls._save_task = asyncio.ensure_future(save_stats())
+    async def _save_progress(self):
+        await self.progress.set(**self.current_progress)
+
+    async def sync_data(self):
+        while True:
+            event = await self.queue.get()
+            self.current_progress = event.progress
+            if not isinstance(event, Event):
+                await self._save_progress()
+                continue
+            ss_list = self.tables_sync_settings_map.get(event.table)
+            if not ss_list:
+                continue
+            async with self.lock:
+                for setting, sync_model in ss_list:
+                    m, _ = self.meili_map[setting]
+                    meilisearch = sync_model.meilisearch
+                    self.stats.setdefault(sync_model.pk, {}).setdefault(event.type, 0)
+                    self.stats[sync_model.pk][event.type] += 1
+                    if not meilisearch.insert_size and not meilisearch.insert_interval:
+                        await m.handle_event(event, setting)
+                        await self._save_progress()
+                    else:
+                        collection = self.collections_map[setting]
+                        collection.add_event(setting, event)
+                        if collection.size >= meilisearch.insert_size:
+                            await m.handle_events(collection)
+                            await self._save_progress()
+
+    async def listen(self):
+        logger.info(
+            f'Start increment sync data from "{self.source.label}" to Meilisearch,'
+            f' tables: {", ".join(self.tables_sync_settings_map.keys())}...'
+        )
+        async for event in self.source_obj:
+            if settings.DEBUG:
+                logger.debug(event)
+            await self.queue.put(event)
+
+    async def start_interval(self, m: Meili, c: EventCollection, interval: int):
+        while True:
+            await asyncio.sleep(interval)
+            try:
+                async with self.lock:  # type: ignore
+                    await m.handle_events(c)
+                    await self._save_progress()
+            except Exception as e:
+                logger.error(f"Error when insert data to Meilisearch: {e}")
+
+    async def run(self):
+        await asyncio.gather(self.save_stats(), self.sync_data(), self.listen())
+
+
+class Scheduler:
+    _tasks: Dict[int, Task] = {}
+
+    @classmethod
+    async def startup(cls):
+        sources = await Source.all()
+        for source in sources:
+            cls._tasks[source.pk] = asyncio.create_task(cls._start_source(source))
+
+    @classmethod
+    async def _start_source(cls, source: Source):
+        async with Runner(source) as runner:
+            await runner.run()
 
     @classmethod
     def shutdown(cls):
-        cls._save_task.cancel()
         for task in cls._tasks.values():
             task.cancel()
 
     @classmethod
     def remove_source(cls, source_id: int):
-        if source_id in cls._tasks:
-            cls._tasks[source_id].cancel()
-            del cls._tasks[source_id]
+        cls._tasks[source_id].cancel()
+        del cls._tasks[source_id]
 
     @classmethod
-    async def restart_source(cls, source: Source, reset_progress: bool = False):
+    async def restart_source(cls, source: Source):
         logger.info(f'Restart source "{source.label}"...')
         source_id = source.pk
         cls.remove_source(source_id)
-        await cls._start_source(source, reset_progress)
+        cls._tasks[source_id] = asyncio.ensure_future(cls._start_source(source))
