eeeffe23e4182e083ab29deedf52e34c4e550320
long2ice <long2ice@gmail.com>
2023-11-12 20:25:57 +0800

    refactor: fastapi lifespan

--- meilisync_admin/app.py
+++ meilisync_admin/app.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from aerich import Command
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
@@ -18,11 +20,24 @@ from meilisync_admin.scheduler import Scheduler
 from meilisync_admin.settings import TORTOISE_ORM, settings
 from meilisync_admin.static import SPAStaticFiles
 
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    init_logging()
+    aerich = Command(TORTOISE_ORM)
+    await aerich.init()
+    await aerich.upgrade(True)
+    await Scheduler.startup()
+    yield
+    Scheduler.shutdown()
+
+
 if settings.DEBUG:
     app = FastAPI(
         title="MeiliSyncAdmin",
         description="MeiliSync admin dashboard",
         debug=settings.DEBUG,
+        lifespan=lifespan,
     )
     app.add_middleware(
         CORSMiddleware,
@@ -38,6 +53,7 @@ else:
         debug=settings.DEBUG,
         redoc_url=None,
         docs_url=None,
+        lifespan=lifespan,
     )
 app.include_router(router, prefix="/api")
 register_tortoise(
@@ -50,21 +66,6 @@ app.add_exception_handler(DoesNotExist, not_exists_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(Exception, exception_handler)
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
-
-
-@app.on_event("startup")
-async def startup():
-    init_logging()
-    aerich = Command(TORTOISE_ORM)
-    await aerich.init()
-    await aerich.upgrade(True)
-    await Scheduler.startup()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    Scheduler.shutdown()
-
 
 if __name__ == "__main__":
     import uvicorn
